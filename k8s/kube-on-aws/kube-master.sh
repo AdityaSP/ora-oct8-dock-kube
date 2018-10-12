69e2971f4871295f1b8a3112ee5fe9dfd818eafc
Aditya <sp.aditya@gmail.com>
2018-10-12 16:17:30 +0530

    Update kube-master.sh

--- k8s/kube-on-aws/kube-master.sh
+++ k8s/kube-on-aws/kube-master.sh
@@ -25,6 +25,7 @@ apt-get install -y kubelet kubeadm kubectl
 
 echo "deploying kubernetes (with calico)..."
 kubeadm init --pod-network-cidr=192.168.0.0/16 # add --apiserver-advertise-address="ip" if you want to use a different IP address than the main server IP
+#kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-address=192.168.33.70
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
 kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/rbac.yaml
